a973ebf1b8bb8ad2836bccd07a0e4f8468c2b5fb
0505Keitan <0505Keitan@gmail.com>
2020-08-31 16:10:02 +0900

    update: ほぼ全て修正

---
+++ components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <img :src="img">
+    <div class="inner">
+      <h1>{{ title }}</h1>
+      <p>
+        <span class="tag" v-for="tag in tags" :key="tag">
+          <span>#{{ tag }}</span>
+        </span>
+      </p>
+      <p>{{ description }}</p>
+      <a :href="link">Link ></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: String,
+    tags: Array,
+    description: String,
+    link: String,
+    img: Boolean,
+    src: String
+  },
+  data() {
+    return {
+      img: this.img
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  border-radius: 10px;
+  margin-bottom: 1rem;
+  background: #bbffc6 none repeat scroll 0% 0%;
+  color: #000; }
+img {
+  display: block;
+  max-width: 50%;
+  border-radius: 5px 5px 0px 0px; }
+div {
+  padding: 10px; }
+h1 {
+  font-size: 35px;
+  margin: 16px -3px; }
+span {
+  margin-right: 5px; }
+p {
+  font-size: 25px;
+    margin: 10px 0; }
+a {
+  text-decoration: none;
+  color: #000;
+  font-size: 20px; }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div v-for="card in cards" :key="card">
+      <Card :title="card.title" :tags="card.tags" :img="require(`@/assets/img/${card.img}`)" :description="card.description" :link="card.link" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+import cards from '@/assets/card.json'
+
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      cards: cards
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #e9ffe0; }
+.container {
+  padding-top: 70px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 1140px; }
+.box {
+  column-width: 400px; }
+</style>
